493ef633442773be0a840ca9620c19325f84a27b
David Rodriguez <davrod0921@gmail.com>
2026-02-03 18:17:43 -0500

    .

--- script.js
+++ script.js
@@ -105,8 +105,8 @@ const StartGame = function () {
     document.querySelector(".Timer").style.opacity = "100%";
     ScoreTxt.textContent = "0";
     ScoreTxt.style.fontSize = "23px";
-    ScoreTxt.style.top = "30px";
-    ScoreTxt.style.left = "20px";
+    ScoreTxt.style.top = "50px";
+    ScoreTxt.style.left = "40px";
     IsGameStarted = true;
     newRNG();
     document.querySelector(".wrapper").style.animation =
