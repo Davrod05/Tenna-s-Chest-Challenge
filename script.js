962833e94080fc54d0e63f8253317e405f9e2012
David Rodriguez <davrod0921@gmail.com>
2026-02-03 20:30:31 -0500

    .

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ const StartGame = function () {
         }
       }, 5 * 1000);
     }, Time * 1000);
-  }, StartTime * 500);
+  }, StartTime * 700);
 
   start.style.opacity = "20%";
   start.disabled = true;
